37ba6552dd398c371cd3483cccb840ec2b4f51e7
RAKSHITPRITAM002 <rakshitpritam002@gmail.com>
2025-05-05 10:26:26 +0530

    CHECK PLEASE

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@
 export { default as About } from './About';
 export { default as AdminDashboard } from './AdminDashboard';
 export { default as Auth } from './Auth';
+export { default as BillingManagement } from './BillingManagement';
 export { default as Dashboard } from './Dashboard';
 export { default as Features } from './Features';
 // Home page component
@@ -9,8 +10,10 @@ export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as Meeting } from './Meeting';
 export { default as MeetingEnhanced } from './MeetingEnhanced';
+export { default as MeetingNew } from './MeetingNew';
 export { default as NotFound } from './NotFound';
 export { default as PasswordReset } from './PasswordReset';
 export { default as Pricing } from './Pricing';
 export { default as Profile } from './Profile';
 export { default as Register } from './Register';
+export { default as SubscriptionCheckout } from './SubscriptionCheckout';
